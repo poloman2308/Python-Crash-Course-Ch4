9dfe0938474c14edb596a91d9432258991901ce9
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-01 22:56:00 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+animals = ['tiger', 'wolf', 'lion']
+for animal in animals:
+	print(animal.title())
+
+print("\n")
+
+for animal in animals:
+	print(animal.title() + "'s are very dangerous animals.")
+
+print("\nAny of these animals wouldn't make a good pet for obvious reasons.")
+
