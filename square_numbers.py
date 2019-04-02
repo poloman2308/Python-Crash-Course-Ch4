06aa89dfcd79d301cfa853d73b64a505249de639
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-01 23:03:53 -0500

    Add files via upload

---
+++ square_numbers.py
@@ -0,0 +1,18 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+square_numbers = []
+for value in range(1, 11):
+	square_number = value**2
+	square_numbers.append(square_number)
+
+print(square_numbers)
+
+# Another way to find the square root of these numbers.
+square_numbers = []
+for value in range(1, 11):
+	square_numbers.append(value**2)
+
+print(square_numbers)
+
