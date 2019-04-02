0f40fd8cbeb5cf22b35a7cdf713d8d1500cfa9a7
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-01 23:08:15 -0500

    Add files via upload

---
+++ counting.py
@@ -0,0 +1,12 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+for numbers in range(1, 21):
+	print(numbers)
+print("\n")	
+
+# Putting these numbers in a list form.
+numbers = list(range(1, 21))
+print(numbers)
+
