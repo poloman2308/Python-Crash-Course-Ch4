4af0b841cfed6a1a0dfe2278258e078a8c74c61c
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-01 22:45:33 -0500

    Add files via upload

---
+++ pizzas.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+pizzas = ['meat lovers', 'buffalo chicken', 'supreme']
+for pizza in pizzas:
+	print(pizza.title())
+
+print("\n")
+
+for pizza in pizzas:
+	print("My favorite pizza is " + pizza.title() + "!")
+
+print("\nMy absolute favoirte kind of food is pizza!")
+
