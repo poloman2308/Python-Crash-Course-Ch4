eaa8828f043528d4fe48ce2e1fc4f78122d41fd9
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-02 22:29:57 -0500

    Add files via upload

---
+++ players.py
@@ -0,0 +1,10 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+	print(player.title())
+
